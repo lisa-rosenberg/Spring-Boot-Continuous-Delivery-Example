c2b233e0537826a3589fb4c6090098c51dadb146
lisa-rosenberg <lisa.rosenberg@outlook.de>
2017-09-29 14:57:57 +0200

    Init Repository

---
+++ src/main/java/com/example/demo/domain/Pet.java
@@ -0,0 +1,32 @@
+package com.example.demo.domain;
+
+public class Pet {
+
+    private String name;
+    private boolean healthy;
+    private String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isHealthy() {
+        return healthy;
+    }
+
+    public void setHealthy(boolean healthy) {
+        this.healthy = healthy;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
